c19ed8264429c095a049fbc0ad3971b97904e553
仲優樹 <yu-ki216m@outlook.jp>
2025-06-26 15:32:21 +0900

    Initial Commit

---
+++ StudyTimerCombine/StudyTimerCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTimerCombineApp.swift
+//  StudyTimerCombine
+//
+//  Created by 仲優樹 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct StudyTimerCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
